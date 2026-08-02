699e8c29a1fe789f5c30e6241997b8c46bec2de5
Mikhail Kachalkov <mikhail.kachalkov@gmail.com>
2026-08-02 23:01:26 +0500

    feat: added lint-staged

--- src/common/services/local-storage-service.ts
+++ src/common/services/local-storage-service.ts
@@ -1,5 +1,5 @@
 export const LocalStorageServiceKeys = {
-  THEME: 'theme',
+  THEME: "theme",
 } as const;
 
 export type LocalStorageServiceKeys =
@@ -9,7 +9,7 @@ class LocalStorageService {
   get<T>(key: LocalStorageServiceKeys | string): T | null {
     const item = localStorage.getItem(key);
 
-    return  item === null ? null : JSON.parse(item);
+    return item === null ? null : JSON.parse(item);
   }
 
   set<T>(key: LocalStorageServiceKeys | string, value: T): void {
